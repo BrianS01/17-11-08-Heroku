c600f02322898dff8bc18774325e63bec33d220b
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-08 10:07:51 -0500

    Heroku

--- src/main/java/MainGlassfish.java
+++ src/main/java/MainGlassfish.java
@@ -9,7 +9,6 @@ import org.glassfish.embeddable.archive.ScatteredArchive;
 
 public class MainGlassfish
 {
-
     public static void main(String[] args) throws Exception {
         String webappDirLocation = "src/main/webapp/";
 
