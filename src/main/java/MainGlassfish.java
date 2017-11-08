3dc7d3068fcc8cc7ed6e8c29ef69a40cea193cb2
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-08 10:12:15 -0500

    Heroku

--- src/main/java/MainGlassfish.java
+++ src/main/java/MainGlassfish.java
@@ -9,11 +9,12 @@ import org.glassfish.embeddable.archive.ScatteredArchive;
 
 public class MainGlassfish
 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
         String webappDirLocation = "src/main/webapp/";
-
         // Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
+        
         if (webPort == null || webPort.isEmpty())
         {
             webPort = "8090";
@@ -24,16 +25,14 @@ public class MainGlassfish
         GlassFishProperties gfProps = new GlassFishProperties();
         gfProps.setPort("http-listener", Integer.valueOf(webPort)); // refer
         gfProps.setProperty("domain-dir", "glassfishDomain");
-        GlassFish glassfish = GlassFishRuntime.bootstrap()
-                .newGlassFish(gfProps);
+        GlassFish glassfish = GlassFishRuntime.bootstrap().newGlassFish(gfProps);
         glassfish.start();
         CommandRunner runner = glassfish.getCommandRunner();
         CommandResult result = runner.run("set-log-level", "javax.enterprise.system.container.web=INFO:javax.enterprise.system.container.ejb=FINEST");
         System.out.println("------output of set log level: " + result.getOutput());
         Deployer deployer = glassfish.getDeployer();
         // Create a scattered web application.
-        ScatteredArchive archive = new ScatteredArchive("myApp",
-                ScatteredArchive.Type.WAR, new File(webappDirLocation));
+        ScatteredArchive archive = new ScatteredArchive("myApp", ScatteredArchive.Type.WAR, new File(webappDirLocation));
         // target/classes directory contains my complied servlets
         archive.addClassPath(new File("target", "classes"));
         deployer.deploy(archive.toURI());
